76b4437342e4b690e714b7f85dd7581eae4d19fe
George Koutsaris <georgekoutsaris@hotmail.com>
2024-03-21 15:28:37 -0400

    #9 added text fields for the player names

--- db-scoreboard/src/components/PlayerinfoPage.jsx
+++ db-scoreboard/src/components/PlayerinfoPage.jsx
@@ -4,17 +4,47 @@ import { useNavigate, useLocation } from 'react-router-dom';
 function PlayerinfoPage() {
     const navigate = useNavigate();
     const location = useLocation();
-    const numOfPlayers = new URLSearchParams(location.search).get('playerCount');
+    const numOfPlayers = location.state?.numOfPlayers;
 
+    const [playerNames, setPlayerNames] = useState(Array(numOfPlayers).fill(''));
+
+    const confirmPlayerInfo = (event) => {
+        event.preventDefault();
+        console.log("Player names:");
+        console.log(playerNames);
+    }
+    const updatePlayerName = (index, value) => {
+        const updatedPlayerNames = [...playerNames];
+        updatedPlayerNames[index] = value;
+        setPlayerNames(updatedPlayerNames);
+    }
+    
     return (
         <>
-        <h1>{numOfPlayers}</h1>
-            <h1>Enter the names of each player</h1>
+            <h1>Enter the names of the {numOfPlayers} player(s)</h1>
+            <br></br>
+            <form onSubmit={confirmPlayerInfo}>
+                {
+                    playerNames.map((value, index) => (
+                        <div key={index}>
+                            <input required
+                                type="text" 
+                                value={value} 
+                                onChange={(e) => updatePlayerName(index,e.target.value)}
+                                placeholder={`Player ${index + 1}'s Name`} 
+                            />
+                            <br/><br/>
+                        </div>
+                        
+                    ))
+                }
+                <br/><br/>
+                <button type="submit">Confirm</button>
+            </form>
             <br></br>
             
-            <button>Confirm</button><br/><br/>
 
-            <button onClick={() => navigate('/player-count')}>Return to Player Count</button>
+            <button onClick={() => navigate('/player-count')}>Change Player Count</button>
         </>
     )
 }
