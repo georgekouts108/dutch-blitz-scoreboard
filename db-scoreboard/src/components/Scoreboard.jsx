007ffc1e0dcd359d0442b9dec4503946128543f6
George Koutsaris <georgekoutsaris@hotmail.com>
2024-04-01 17:12:08 -0400

    #20 component for a player's record in the scoreboard

--- db-scoreboard/src/components/Scoreboard.jsx
+++ db-scoreboard/src/components/Scoreboard.jsx
@@ -2,20 +2,37 @@ import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from 'react-router-dom';
 import { Ranks } from "../assets/ranks";
 
+import SBoardRow from "./SBoardRow";
+
 function Scoreboard() {
     const navigate = useNavigate();
     const location = useLocation();
 
-    const [players, setPlayers] = useState(location.state?.players);
+    const dummyPlayers = [
+        {id: 0, name: 'Dimitri', num: 1, grandTotal: 0, rank: '1st'},
+        {id: 1, name: 'George', num: 2, grandTotal: 0, rank: '1st'},
+        {id: 10, name: 'Michael', num: 3, grandTotal: 0, rank: '1st'}
+    ]
+
+    const [players, setPlayers] = useState(dummyPlayers); // this is temporary
+    //const [players, setPlayers] = useState(location.state?.players);
     const [pointsToWin, setPointsToWin] = useState(location.state?.pointsToWin);
     const [roundCount, setRoundCount] = useState(0);
 
     return (
         <>
             <h1>Scoreboard</h1>
-            
             <h2>Be the first to reach {pointsToWin} points or more!</h2>
-            <h3>{players[3].name}</h3>
+            <div>
+                {
+                    players.map((player, index) => (
+                        <div key={index} className="row">
+                            <SBoardRow p_rank={player.rank} p_id={player.id} p_name={player.name} p_grandTotal={player.grandTotal} />
+                        </div> 
+                    ))
+                    
+                }<br></br>
+            </div>
             <button>Quit Game</button>
             <button>Start Next Round!</button>
         </>
