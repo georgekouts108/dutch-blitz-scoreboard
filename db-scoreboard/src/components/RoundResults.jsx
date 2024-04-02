ab9b870353ba30a86d3bedd7872e7a65fa793e16
George Koutsaris <georgekoutsaris@hotmail.com>
2024-04-01 23:36:40 -0400

    #23 (BUGFIX) added controls for entering and confirming round scores per player

--- db-scoreboard/src/components/RoundResults.jsx
+++ db-scoreboard/src/components/RoundResults.jsx
@@ -2,9 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from 'react-router-dom';
 import { cardSymbols } from "../assets/card_symbols/card_symbols";
 
-import RoundResultsCard from "./RoundResultsCard";
-
-
 function RoundResults() {
     const navigate = useNavigate();
     const location = useLocation();
@@ -12,44 +9,111 @@ function RoundResults() {
     const roundNum = location.state?.roundNumber;
     const players = location.state?.players;
 
-    // this array will eventually update the grand total fields in "player"
-    const [roundResults, setRoundResults] = useState(
-        players.map(player => {
-            return {
-                pid: player.id, 
+    
+    const _ri = []
+    if (players !== undefined){
+       
+        for (let p = 0; p < players.length; p++) {
+            const _next = {
+                pid: players[p].id, 
+                pname: players[p].name,
+                prank: players[p].rank,
+                pgrandTotal: players[p].grandTotal,
+                pnum: players[p].num,
                 dutch:0, 
                 blitz:0, 
-                roundTotal:0
+                scoreConfirmed:false
             }
-        })
-    )
+            _ri.push(_next)
+        }
+    }
+   
+ 
+    // this array will eventually update the grand total fields in "player"
+    const [roundResults, setRoundResults] = useState(_ri)
+
+    const [scoreConfirmCount, setScoreConfirmCount] = useState(0)
+    const [playerCount, setPlayerCount] = useState(players.length)
+    
+    const updateCount = (pnum, category, amount, up) => {
+        const tempRoundResults = [...roundResults]
+        
+        if (category === 'dutch') {
+            if (up) {
+                tempRoundResults[pnum - 1].dutch = Math.min(40, tempRoundResults[pnum - 1].dutch + amount)
+            }
+            else {
+                tempRoundResults[pnum - 1].dutch = Math.max(0, tempRoundResults[pnum - 1].dutch - amount)
+            }
+        }
+        else if (category === 'blitz') {
+            if (up) {
+                tempRoundResults[pnum - 1].blitz = Math.min(10, tempRoundResults[pnum - 1].blitz + amount)
+            }
+            else {
+                tempRoundResults[pnum - 1].blitz = Math.max(0, tempRoundResults[pnum - 1].blitz - amount)
+            }
+        }
+        
+        setRoundResults(tempRoundResults)
+        console.log(roundResults)
+    }
+
+    const confirmPlayerScore = (pnum) => {
+        const tempRoundResults = [...roundResults]
+        let roundScore = tempRoundResults[pnum - 1].dutch - (2 * tempRoundResults[pnum - 1].blitz)
+        tempRoundResults[pnum - 1].pgrandTotal = tempRoundResults[pnum - 1].pgrandTotal + roundScore
+        tempRoundResults[pnum - 1].scoreConfirmed = true
+        setScoreConfirmCount(scoreConfirmCount + 1)
+        setRoundResults(tempRoundResults)
+    }
 
     const confirmResults = () => {
         console.log("todo")
         console.log(roundResults)
     }
-
-    // const updateResults = (index, value) => {
-    //     const updatedPlayerNames = [...playerNames];
-    //     updatedPlayerNames[index] = value;
-    //     setPlayerNames(updatedPlayerNames);
-    // }
+    
     return (
         <>
             <h1>Enter all the results of Round {roundNum}</h1>
 
             <div>
                 {
-                    players.map((player, index) => (
-                        <div key={player.id} className="row">
-                            <RoundResultsCard p_id={player.id} p_name={player.name}/>
+                    roundResults.map((player) => (
+                        <div key={player.pid} className="row">
+                            <div> 
+                                    {
+                                        <div>
+                                            <img src={cardSymbols[player.pid]} alt="hhh" height={60} width={60}/>
+                                            <b>{player.pname}</b>
+                                            <h3>Number of Dutch Cards: {roundResults[player.pnum - 1].dutch}</h3>
+                                            
+                                            <button disabled={player.scoreConfirmed===true || roundResults[player.pnum - 1].dutch===0} onClick={() => updateCount(player.pnum,'dutch',1,false)} >-1</button>
+                                            <button disabled={player.scoreConfirmed===true || roundResults[player.pnum - 1].dutch===0} onClick={() => updateCount(player.pnum,'dutch',5,false)}>-5</button>
+                                            <button disabled={player.scoreConfirmed===true || roundResults[player.pnum - 1].dutch===40} onClick={() => updateCount(player.pnum,'dutch',5,true)}>+5</button>
+                                            <button disabled={player.scoreConfirmed===true || roundResults[player.pnum - 1].dutch===40} onClick={() => updateCount(player.pnum,'dutch',1,true)}>+1</button>
+                                            
+                                        
+                                            <h3>Number of Blitz Cards: {roundResults[player.pnum - 1].blitz}</h3>
+
+                                            <button disabled={player.scoreConfirmed===true || roundResults[player.pnum - 1].blitz===0} onClick={() => updateCount(player.pnum,'blitz',1,false)} >-1</button>
+                                            <button disabled={player.scoreConfirmed===true || roundResults[player.pnum - 1].blitz===10} onClick={() => updateCount(player.pnum,'blitz',1,true)}>+1</button>
+                                            
+                                        
+                                            <br/><br/>
+                                        </div>
+                                    }
+                                    <button disabled={player.scoreConfirmed===true} onClick={() => confirmPlayerScore(player.pnum)}>Confirm Score</button>
+                                    <br/><br/>
+                                    <hr></hr>
+                            </div>
                         </div> 
                     ))
-                    
-                }<br></br>
+                }
+                <br></br>
             </div>
 
-            <button onClick={confirmResults}>Confirm Results</button>
+            <button disabled={scoreConfirmCount < playerCount} onClick={confirmResults}>Continue</button>
         </>
     )
 }
