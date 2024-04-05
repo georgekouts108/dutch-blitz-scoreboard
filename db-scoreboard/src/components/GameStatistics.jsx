277ffbaa16c6900429fcc191b4856fe4c9ad3c74
George Koutsaris <georgekoutsaris@hotmail.com>
2024-04-04 22:24:38 -0400

    #34 wrote an algorithm that sorts all players by blitz count

--- db-scoreboard/src/components/GameStatistics.jsx
+++ db-scoreboard/src/components/GameStatistics.jsx
@@ -11,6 +11,24 @@ function GameStatistics() {
     const pointsToWin = location.state?.pointsToWin;
     const championIDs = location.state?.championIDs;
 
+    const temp = [...players]
+    const blitzCounts = []
+    for (let t = 0; t < temp.length; t++) {
+        blitzCounts.push([temp[t].id, temp[t].blitzCount])
+    }
+    blitzCounts.sort((a, b) => b[1] - a[1]);
+    const sortedPlayersByBlitzCount = []
+    for (let bc = 0; bc < blitzCounts.length; bc++) {
+        for (let p = 0; p < players.length; p++) {
+            if (players[p].id === blitzCounts[bc][0]) {
+                sortedPlayersByBlitzCount.push(players[p])
+                break;
+            }
+        }
+    }
+
+    const [blitzCountSorting, setBlitzCountSorting] = useState(sortedPlayersByBlitzCount)
+    
     const goToFinalScores = () => {
         navigate('/game-conclusion', { 
             state: { 
@@ -21,9 +39,12 @@ function GameStatistics() {
             } 
         });
     }
+    
     return (
         <>
             <h1>Game Statistics</h1>
+            <h2>Scores of Each Player</h2>
+            <h2>Blitz Count Per Player</h2>
             <button onClick={goToFinalScores}>Back To Final Scores</button>
         </>
     )
