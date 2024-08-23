ce05aa751c11281eedaf439a5c69a189c234a5d3
George Koutsaris <georgekoutsaris@hotmail.com>
2024-08-23 18:07:35 -0400

    #38 gave a custom style to 1st-place score banners

--- db-scoreboard/src/components/ScoreBanner.jsx
+++ db-scoreboard/src/components/ScoreBanner.jsx
@@ -6,8 +6,12 @@ import { rankIcons } from "../assets/rank_images/ranks";
 function ScoreBanner(props) {
     let rank_str = props.p_rank+""
     let rank_index = parseInt(rank_str.substring(0, rank_str.length-2)) - 1
+    
+    let border = "1px solid " + (rank_index === 0 ? "#d79a0b" : "#ffffffb4");
+    let box_shadow = "5px 5px 5px " + (rank_index === 0 ? "#d79a0b" : "#ffffffb4");
+    let bg_color = (rank_index === 0 ? "#ffd69459" : "")
     return (
-        <div className="score-banner">
+        <div className="score-banner" style={{border:border, boxShadow:box_shadow,backgroundColor:bg_color}}>
             <img className="rank-logo" src={rankIcons[rank_index]} alt="icon"></img>
             <img className="player-icon" src={cardSymbols[props.p_id]} alt="icon"></img>
             <h1 className="player-name">{props.p_name}</h1>
