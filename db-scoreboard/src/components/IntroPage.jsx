7f795cec070ba82b89e2408024e4d0e5e62a032d
George Koutsaris <georgekoutsaris@hotmail.com>
2024-08-24 16:58:16 -0400

    #41 #42 added dutch blitz farmer icons to intro title, also changed favicon

--- db-scoreboard/src/components/IntroPage.jsx
+++ db-scoreboard/src/components/IntroPage.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
 import dbLogo from '../assets/images/db_logo.gif'
+import blitzFemale from '../assets/images/blitz_female.png'
+import blitzMale from '../assets/images/blitz_male.png'
+import '../styles/introPage.css'
 
 function IntroPage() {
     const navigate = useNavigate();
     document.title = "Home - Dutch Blitz Scoreboard"
     return (
         <>
-            <img src={dbLogo}/>
-            <h2>Scoreboard for the vonderful goot game</h2>
+            <img src={blitzMale} className="icon-farmer"/><img src={dbLogo}/><img className="icon-farmer" src={blitzFemale}/>
+            <h2>A Vonderful Goot Scoreboard</h2>
             <button onClick={()=>navigate('/player-info', {
                 state:{editingNames:false}
             }
